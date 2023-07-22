0651e656017c0c566e97eee7691dc34baf75c305
Mobterest Studio <collins.odhiambo@cellulant.io>
2023-07-22 20:26:23 +0300

    one screen design ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:slider_button/slider_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +9,159 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        textTheme: Theme.of(context)
+            .textTheme
+            .apply(fontFamily: GoogleFonts.poppins().fontFamily),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height * 0.45,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 8.0, left: 8.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.arrow_back),
+                        Spacer(),
+                        Icon(Icons.menu)
+                      ],
+                    ),
+                  ),
+                  Stack(children: [
+                    const Align(
+                        alignment: Alignment.topCenter,
+                        child: Icon(
+                          Icons.pin_drop,
+                          size: 150,
+                          color: Color(0xffF0F0F0),
+                        )),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 90.0, right: 20, left: 20),
+                      child: Image.asset(
+                        "assets/car.png",
+                        width: 380,
+                      ),
+                    ),
+                  ]),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(right: 8.0, left: 8.0, top: 15),
+                    child: Row(
+                      children: [
+                        RichText(
+                            text: const TextSpan(children: [
+                          WidgetSpan(
+                              child: Icon(
+                            Icons.apple,
+                            size: 50,
+                          )),
+                          TextSpan(
+                              text: "Park",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 25))
+                        ])),
+                        Spacer(),
+                        const Text(
+                          "\$3 / hr",
+                          style: TextStyle(fontSize: 18),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Container(
+              color: const Color(0xff252525),
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "End Time",
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Today",
+                                style: TextStyle(color: Colors.black),
+                              )),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Tomorrow",
+                                style: TextStyle(color: Colors.white),
+                              )),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Later",
+                                style: TextStyle(color: Colors.white),
+                              ))
+                        ],
+                      ),
+                      const Text(
+                        "10:40 pm",
+                        style: TextStyle(fontSize: 50, color: Colors.white),
+                      ),
+                      RichText(
+                          text: const TextSpan(children: [
+                        WidgetSpan(
+                            child: Icon(
+                          Icons.alarm,
+                          size: 30,
+                          color: Colors.white,
+                        )),
+                        TextSpan(
+                            text: "Alert me 15 minutes before",
+                            style: TextStyle(color: Colors.white, fontSize: 16))
+                      ])),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 30.0),
+                          child: SliderButton(
+                              backgroundColor: Color(0xffd3d3d3),
+                              action: () {},
+                              label: const Text(
+                                "Slide to park",
+                                style: TextStyle(
+                                    color: Color(0xff4a4a4a),
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 17),
+                              ),
+                              icon: const Icon(
+                                Icons.keyboard_double_arrow_right,
+                                size: 30,
+                              )),
+                        ),
+                      )
+                    ]),
+              ),
+            )
           ],
-        ),
+        )),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      debugShowCheckedModeBanner: false,
     );
   }
 }
